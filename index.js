66a9792632d7b4dbeff5257a11ad5eb798f1b1e5
Your Name <your-email@example.com>
2026-03-26 10:14:03 +0530

    Remove node_modules and add gitignore

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const url="mongodb://localhost:27017";
 const client = new MongoClient(url);
 
 const app = express();
+app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 
 client.connect().then((connection)=>{
@@ -22,6 +23,18 @@ client.connect().then((connection)=>{
         const students = await collection.find().toArray();
        res.render('students',{students})
     });
+
+    app.get('/add',(req,res)=>{
+        res.render(add-student)
+    })
+    app.post("/add-student",async(req,res)=>{
+       // console.log(req.body);
+        const collection = db.collection("students");
+        const result=await collection.insertOne(req.body);
+        console.log(result)
+        // const students = await collection.find().toArray();
+        res.send("data saved");
+    });
 })
 
 
@@ -36,4 +49,4 @@ client.connect().then((connection)=>{
    
 // });
 
-app.listen(3200);
+app.listen(3100);
